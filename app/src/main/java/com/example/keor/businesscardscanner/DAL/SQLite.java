44a25caa422f184f7b86bda0db5e2ef932bc957b
Kevin Ørskov <keor@bws.dk>
2015-08-27 10:27:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/keor/businesscardscanner/DAL/SQLite.java
@@ -0,0 +1,8 @@
+package com.example.keor.businesscardscanner.DAL;
+
+/**
+ * Created by keor on 27-08-2015.
+ */
+public class SQLite {
+
+}
---
+++ app/src/main/java/com/example/keor/businesscardscanner/GUI/LoginActivity.java
@@ -0,0 +1,58 @@
+package com.example.keor.businesscardscanner.GUI;
+
+import android.graphics.Color;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.example.keor.businesscardscanner.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        findViews();
+        initToolbar();
+
+    }
+
+    private void findViews() {
+        toolbar = (Toolbar) findViewById(R.id.tool_bar);
+    }
+
+    private void initToolbar() {
+        toolbar.setTitle("Login");
+        toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setNavigationIcon(R.drawable.ic_login);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
